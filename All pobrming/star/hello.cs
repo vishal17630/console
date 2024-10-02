52265cd455dda27b1888c94c4673c4acc51a977e
vishal17630 <168903778+vishal17630@users.noreply.github.com>
2024-10-02 19:24:41 +0530

    five

---
+++ All pobrming/star/hello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_pobrming.star
+{
+    internal class hello
+    {
+        public void Program()
+        {
+
+            {
+                Console.WriteLine(3 + 3);
+            }
+        }
+    }
+}
+    
