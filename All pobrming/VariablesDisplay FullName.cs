ce2ffef1f290bece292c50d045b788285f83a049
vishal17630 <168903778+vishal17630@users.noreply.github.com>
2024-10-03 16:08:56 +0530

    VariablesDisplay

---
+++ All pobrming/VariablesDisplay FullName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_pobrming
+{
+    public class VariablesDisplay_FullName
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Vishal ";
+            string fathersName = "Atul ";
+            string lastName = "Chaudhari";
+            string fullName = FirstName + fathersName + lastName;
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ All pobrming/VariablesDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace All_pobrming
+//{
+//    public class VariablesDisplay
+//    {
+//        static void Main (string[] args)
+//        {
+//            string Name = "Charu";
+//            Console.WriteLine("Hello " + Name);
+//            Console.ReadLine();
+//        }
+//    }
+//}
